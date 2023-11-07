9301af310f5daa3e558ec2be3bcd3ffebabf216c
Srajan B Shetty <96736901+entity-cosmos@users.noreply.github.com>
2023-11-07 18:08:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -29,6 +30,7 @@ function App() {
   }, []);
 
   return (
+    <Analytics />
     <Router>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
